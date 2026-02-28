bd03aeb025168e3b4bbbadf6448a204831055de7
clrlbhanu <bhanuverma2605@gmail.com>
2026-02-28 17:05:34 +0530

    video change css change admin removal page header change

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,20 +1,15 @@
-import '../css/page_header.css';
+import "../css/page_header.css";
 
 export default function PageHeader({ title, path, name }) {
-
-    return (
-        <div className="page-header" >
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h2>{title}</h2>
-                    </div>
-                    <div className="col-12">
-                        <a href="/">Home</a>
-                        <a href={path}>{name}</a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="page-header">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h2>{title}</h2>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
